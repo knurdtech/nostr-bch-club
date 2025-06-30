55e175b941f7f349e62a15bf774a8d4767b56f3b
knurdtech <knurd@knurdtech.com>
2025-06-30 14:02:44 -0500

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,6 @@ import tailwind from '@astrojs/tailwind';
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind()],
-  site: 'https://nostrbch.org',
+  site: 'https://nostrbch.club',
   base: '/',
 });
